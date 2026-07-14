d3f5db2caa85b2abbe299a40bc4bb5b75d9cd911
josuuua <166282046+josuuua@users.noreply.github.com>
2026-07-14 11:13:17 +0900

    chore: refresh backend setup

---
+++ backend/src/test/java/CAI/Notice/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package CAI.Notice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
